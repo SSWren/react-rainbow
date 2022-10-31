aeeb362c41bbabe7b2cdb7a9ee8686400bc1692a
SSWren <sydney.wrenwick@gmail.com>
2022-10-31 14:55:52 -0400

    3 components added, complete

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
+import './App.css'
+
+function App () {
+  let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+  
+  let colorMap = colors.map((color, i) => {
+  
+  return (
+      <ColorBlock color={color} />
+    )
+  });
+
+  const addColor= (newColor) => {
+    setColors([...colors, newColor])
+  }
+
+  return (
+    <div>
+      {colorMap}
+      <ColorForm addColor={addColor} />
+    </div>
+  )
+}
+
+export default App;
